50085d6f057f0e602de4a04f440fe87df49b3bc1
ayegbakenneth <ayegbakenneth@gmail.com>
2023-11-02 10:50:41 -0700

    Write a beautiful code that passes the Betty checks

---
+++ test_file.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Program starting point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char first_name[40];
+	char last_name[40];
+	int store;
+
+	write(STDOUT_FILENO, "Type your first name: ", 23);
+	store = read(STDIN_FILENO, first_name, sizeof(first_name) - 1);
+	
+	if (store <= 0)
+	{
+		perror("Error, can't read name");
+		return (1);
+	}
+	first_name[store - 1] = '\0'; 
+	write(STDOUT_FILENO, "Type your last name: ", 22);
+	store = read(STDIN_FILENO, last_name, sizeof(last_name) - 1);
+	if (store <= 0)
+	{
+		perror("Error can't read input");
+		return (1);
+	}
+	last_name[store - 1] = '\0';
+	char display[100];
+	int first_lastname = 0;
+	int b;
+
+	for (b = 0; first_name[b] != '\0'; b++)
+	{
+		display[first_lastname++] = first_name[b];
+	}
+	display[first_lastname++] = ' ';
+	for (b = 0; last_name[b] != '\0'; b++)
+	{
+		display[first_lastname++] = last_name[b];
+	}
+	display[first_lastname++] = '\n';
+	store = write(STDOUT_FILENO, display, first_lastname);
+	if (store <= 0)
+	{
+		perror("Error writing display");
+		return (1);
+	}
+	return (0);
+}
