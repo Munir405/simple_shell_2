645c68028502f3140ce3d75ebb561e89d3d572a7
Munir405 <deelagbrainy2@gmail.com>
2023-11-14 04:52:17 -0800

    Prompt me

--- print.c
+++ print.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * ken_display - This is used to display an output
  * @title: The output
@@ -8,4 +9,13 @@
 void ken_display(const char *title)
 {
 	write(STDOUT_FILENO, title, strlen(title));
+}
+
+/**
+ * show_prompt - Display the shell prompt
+ */
+
+void show_prompt(void)
+{
+	ken_display("kenny_shell$ ");
 }
